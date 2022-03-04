73dc3be28d477de919f0d0232274b5e6d341205b
Maier Raul <maierraul96@gmail.com>
2022-03-04 21:02:53 +0200

    Use .env file for env vars (#4)
    Co-authored-by: raul.maier <raul.maier@ly.st>

--- syneto_translate/translate.py
+++ syneto_translate/translate.py
@@ -3,20 +3,23 @@ from google.cloud import translate
 from google.oauth2 import service_account
 import json
 
+from syneto_translate import PROJECT_ID, SERVICE_ACCOUNT_FILE
+
 
 def load_credentials() -> service_account.Credentials:
-    with open('syneto_translate/account_cred.json') as file:
+    print(PROJECT_ID, SERVICE_ACCOUNT_FILE)
+    with open(SERVICE_ACCOUNT_FILE) as file:
         credentials_json = json.load(file)
         return service_account.Credentials.from_service_account_info(credentials_json)
 
 
-def translate_text(text, dest_lang: str, project_id="syneto-test-343021") -> str:
+def translate_text(text: str, dest_lang: str) -> str:
     """Translating Text."""
     client = translate.TranslationServiceClient(credentials=load_credentials())
 
     location = "global"
 
-    parent = f"projects/{project_id}/locations/{location}"
+    parent = f"projects/{PROJECT_ID}/locations/{location}"
 
     # https://cloud.google.com/translate/docs/supported-formats
     response = client.translate_text(
